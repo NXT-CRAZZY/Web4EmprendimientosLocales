553d4380479e4ae116d9e9a529146508ba025fc9
RubenDario <182975@unsaac.edu.pe>
2025-05-03 10:56:02 -0500

    Subiendo Mi Proyecto

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Marketplace</h1>
+      {/* Aquí irían rutas, navbar, componentes */}
+    </div>
+  );
+}
+
+export default App;
+
+
